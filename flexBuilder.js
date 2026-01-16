042ce854c36503b2fa9fb0e545c0d745e2a56db3
Joongi-J <thitikorn.p@hotmail.com>
2026-01-16 16:26:40 +0700

    Update flexBuilder.js

--- flexBuilder.js
+++ flexBuilder.js
@@ -1,4 +1,4 @@
-function buildStockFlex(symbol, q) {
+function buildPriceFlex(symbol, q) {
   const diff = q.c - q.pc;
   const up = diff >= 0;
 
@@ -14,9 +14,9 @@ function buildStockFlex(symbol, q) {
           { type: 'text', text: symbol, size: 'xl', weight: 'bold' },
           {
             type: 'text',
-            text: `${up ? '▲' : '▼'} ${q.c} USD`,
-            color: up ? '#00C853' : '#D50000',
-            size: 'lg'
+            text: `${up ? '▲' : '▼'} ${q.c}`,
+            size: 'lg',
+            color: up ? '#00C853' : '#D50000'
           }
         ]
       }
@@ -24,23 +24,4 @@ function buildStockFlex(symbol, q) {
   };
 }
 
-function buildHeatmapFlex(data) {
-  return {
-    type: 'flex',
-    altText: 'Crypto Heatmap',
-    contents: {
-      type: 'bubble',
-      body: {
-        type: 'box',
-        layout: 'vertical',
-        contents: data.map(c => ({
-          type: 'text',
-          text: `${c.symbol} ${c.change > 0 ? '▲' : '▼'} ${c.change}%`,
-          color: c.change > 0 ? '#00C853' : '#D50000'
-        }))
-      }
-    }
-  };
-}
-
-module.exports = { buildStockFlex, buildHeatmapFlex };
+module.exports = { buildPriceFlex };
